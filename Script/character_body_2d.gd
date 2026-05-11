661cda21a917718c96b4489f51f3123ab4a09b88
dmytro <go.dimkaking@gmail.com>
2026-05-11 17:29:04 +0200

    update

---
+++ Script/character_body_2d.gd
@@ -0,0 +1,74 @@
+extends CharacterBody2D
+
+enum {
+	DOWN,
+	UP,
+	LEFT,
+	RIGHT
+}
+
+@onready var anim = $AnimatedSprite2D
+
+@export var speed := 100.0
+
+var target_position: Vector2
+var moving := false
+var idle_dir = DOWN
+
+
+func _ready():
+	target_position = global_position
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			target_position = get_global_mouse_position()
+			moving = true
+
+
+func _physics_process(delta: float) -> void:
+	if moving:
+		var direction = global_position.direction_to(target_position)
+		velocity = direction * speed
+
+		play_walk_animation(direction)
+
+		if global_position.distance_to(target_position) < 5:
+			velocity = Vector2.ZERO
+			moving = false
+			idle()
+	else:
+		velocity = Vector2.ZERO
+		idle()
+
+	move_and_slide()
+
+
+func play_walk_animation(direction: Vector2):
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			anim.play("run_right")
+			idle_dir = RIGHT
+		else:
+			anim.play("run_left")
+			idle_dir = LEFT
+	else:
+		if direction.y > 0:
+			anim.play("run_down")
+			idle_dir = DOWN
+		else:
+			anim.play("run_up")
+			idle_dir = UP
+
+
+func idle():
+	match idle_dir:
+		DOWN:
+			anim.play("idle_down")
+		UP:
+			anim.play("idle_up")
+		LEFT:
+			anim.play("idle_left")
+		RIGHT:
+			anim.play("idle_right")
